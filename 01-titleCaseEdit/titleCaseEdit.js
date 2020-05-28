34b8f6edd5e3b17d4d2da3311f19e52a1d223f34
shipu11b <mshipu143@gmail.com>
2020-05-28 19:42:56 -0400

    intial start

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,6 +1,8 @@
 function titleCaseEdit(title) {
-  // Insert code here;
+    // Insert code here;
+    console.log(title);
 }
+titleCaseEdit("this is new page");
 
 // Do not edit this line;
 module.exports = titleCaseEdit;
